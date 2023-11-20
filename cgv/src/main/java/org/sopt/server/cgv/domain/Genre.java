d812eda3b8553838a68c911d3779c85e6bb01e5c
Choi YoungLyn <joeuni0129@gmail.com>
2023-11-20 21:22:27 +0900

    [Feat] #3 - 엔티티에 필요한 Enum 추가

---
+++ cgv/src/main/java/org/sopt/server/cgv/domain/Genre.java
@@ -0,0 +1,14 @@
+package org.sopt.server.cgv.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Genre {
+    ANIMATION("애니메이션"),
+    ACTION("액션"),
+    HORROR("공포");
+
+    private final String name;
+}
---
+++ cgv/src/main/java/org/sopt/server/cgv/domain/ScreenType.java
@@ -0,0 +1,17 @@
+package org.sopt.server.cgv.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ScreenType {
+    ALL("전체"),
+    NORMAL("일반관"),
+    COMFORT("컴포트관"),
+    IMAX("IMAX관"),
+    GOLDCLASS("GOLD CLASS관");
+
+    private final String name;
+}
+
