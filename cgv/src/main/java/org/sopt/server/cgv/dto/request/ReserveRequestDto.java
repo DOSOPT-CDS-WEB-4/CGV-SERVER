11c5367169b67814ce0b1abfb99ad8cf4cfe9298
Choi YoungLyn <joeuni0129@gmail.com>
2023-11-22 02:51:04 +0900

    [Chore] #8 - RequsetDto 형식 변경

--- cgv/src/main/java/org/sopt/server/cgv/dto/request/ReserveRequestDto.java
+++ cgv/src/main/java/org/sopt/server/cgv/dto/request/ReserveRequestDto.java
@@ -1,5 +1,8 @@
 package org.sopt.server.cgv.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record ReserveRequestDto (
+        @JsonProperty("schedule_id")
         Long id
 ) {}
