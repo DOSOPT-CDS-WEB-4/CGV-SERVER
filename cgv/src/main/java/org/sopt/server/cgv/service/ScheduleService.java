3636a8b7f532f15b2df206511345cacd0723398b
Chan531 <chan657312@gmail.com>
2023-11-23 17:41:42 +0900

    [Feat] #15 - 날짜 필터링 기능 추가

--- cgv/src/main/java/org/sopt/server/cgv/service/ScheduleService.java
+++ cgv/src/main/java/org/sopt/server/cgv/service/ScheduleService.java
@@ -9,6 +9,7 @@ import org.sopt.server.cgv.repository.ScheduleRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -29,14 +30,14 @@ public class ScheduleService {
         return ReserveResponseDto.of(schedule);
     }
 
-    public List<MovieScreenScheduleResponseDto> getMovieScreenScheduleInfo(List<Long> screenIdList, int runningTime) {
-        List<Schedule> schedules = scheduleRepository.findByScreenIdIn(screenIdList);
-        return schedules.stream()
+    public List<MovieScreenScheduleResponseDto> getMovieScreenScheduleInfo(List<Long> screenIdList, int runningTime, LocalDate date) {
+        return scheduleRepository.findByScreenIdIn(screenIdList)
+                .stream()
+                .filter(schedule -> schedule.getStartTime().toLocalDate().equals(date))
                 .map(schedule -> MovieScreenScheduleResponseDto.of(
                         schedule,
                         schedule.getStartTime().plusHours(runningTime / HOUR).plusMinutes(runningTime % HOUR),
                         schedule.getStartTime().isBefore(LocalDateTime.now())
                 )).toList();
     }
-
 }
