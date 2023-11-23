256425978f3b182454f2a7b401ee0fa107e46f8d
Chan531 <chan657312@gmail.com>
2023-11-23 14:06:48 +0900

    [Feat] #5 - 영화 상영관별 상영시간 dto

---
+++ cgv/src/main/java/org/sopt/server/cgv/dto/response/MovieScreenScheduleResponseDto.java
@@ -0,0 +1,28 @@
+package org.sopt.server.cgv.dto.response;
+
+import org.sopt.server.cgv.domain.Schedule;
+import org.sopt.server.cgv.domain.ScreenType;
+
+import java.time.LocalDateTime;
+
+public record MovieScreenScheduleResponseDto(
+        ScreenType screenType,
+        String place,
+        LocalDateTime startTime,
+        LocalDateTime endTime,
+        int totalSeats,
+        int emptySeats,
+        boolean reservationAvailability
+) {
+    public static MovieScreenScheduleResponseDto of(Schedule schedule, LocalDateTime endTime, boolean reservationAvailability) {
+        return new MovieScreenScheduleResponseDto(
+                schedule.getScreen().getScreenType(),
+                schedule.getScreen().getPlace(),
+                schedule.getStartTime(),
+                endTime,
+                schedule.getTotalSeats(),
+                schedule.getEmptySeats(),
+                reservationAvailability
+        );
+    }
+}
