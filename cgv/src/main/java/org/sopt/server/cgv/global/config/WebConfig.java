8407fb17581fff52401f0943a5847dd6dbf7e209
Chan531 <chan657312@gmail.com>
2023-11-27 18:30:58 +0900

    [Refactor] #27 - 프론트 로컬 url cors에 추가

--- cgv/src/main/java/org/sopt/server/cgv/global/config/WebConfig.java
+++ cgv/src/main/java/org/sopt/server/cgv/global/config/WebConfig.java
@@ -29,7 +29,7 @@ public class WebConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));
